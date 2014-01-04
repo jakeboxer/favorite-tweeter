ec5c7d26e5e13119bb193f369f33e8d6859b22f1
Jake Boxer <jake@github.com>
2014-01-03 18:20:17 -0800

    Add twitter gem

---
+++ vendor/ruby/2.0.0/gems/twitter-5.4.1/lib/twitter/version.rb
@@ -0,0 +1,15 @@
+module Twitter
+  class Version
+    MAJOR = 5
+    MINOR = 4
+    PATCH = 1
+    PRE = nil
+
+    class << self
+      # @return [String]
+      def to_s
+        [MAJOR, MINOR, PATCH, PRE].compact.join('.')
+      end
+    end
+  end
+end
