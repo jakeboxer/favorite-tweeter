d737d9fb9a6a03d54c515f64a6f1f2ab5f3dcff7
Jake Boxer <jake@github.com>
2014-01-03 21:44:35 -0800

    Show users sorted by favs and retweets

--- app/controllers/tweeters_controller.rb
+++ app/controllers/tweeters_controller.rb
@@ -11,6 +11,31 @@ class TweetersController < ApplicationController
 
   def show
     @tweeter = Tweeter.find_by!(:screen_name => params[:screen_name])
-    @tweets = TWITTER.user_timeline(@tweeter.screen_name)
+
+    faved_tweets      = TWITTER.favorites(@tweeter.screen_name, :count => 100)
+    retweets          = TWITTER.retweeted_by_user(@tweeter.screen_name, :count => 200)
+    oldest_tweet_time = 3.months.ago
+
+    faved_users     = Hash.new(0)
+    retweeted_users = Hash.new(0)
+
+    faved_tweets.each do |tweet|
+      break unless tweet.created_at >= oldest_tweet_time
+
+      faved_users[tweet.user.screen_name] += 1
+
+      if tweet.retweet?
+        faved_users[tweet.retweeted_status.user.screen_name] += 1
+      end
+    end
+
+    retweets.each do |tweet|
+      break unless tweet.created_at >= oldest_tweet_time
+
+      retweeted_users[tweet.retweeted_status.user.screen_name] += 1
+    end
+
+    @tweeters_by_favs = faved_users.sort_by { |_, favs| favs }.reverse
+    @tweeters_by_rts  = retweeted_users.sort_by { |_, rts| rts }.reverse
   end
 end
