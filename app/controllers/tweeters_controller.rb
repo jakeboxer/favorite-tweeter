78c1dc7a610e2d09494e2849fd6bbffdf112cef6
Jake Boxer <jake@github.com>
2014-01-05 03:57:48 -0800

    Add AJAX polling for stats calculation
    * Add TweetersController#stats endpoint for AJAX poller

--- app/controllers/tweeters_controller.rb
+++ app/controllers/tweeters_controller.rb
@@ -21,4 +21,20 @@ class TweetersController < ApplicationController
       end
     end
   end
+
+  def stats
+    tweeter = Tweeter.find_by!(:screen_name => params[:id].strip)
+
+    respond_to do |format|
+      format.js do
+        if tweeter.calculation_required?
+          render :text => "Stats calculation required", :status => 403
+        elsif tweeter.stats_job_queued?
+          render :text => "Stats calculation processing", :status => 202
+        else
+          render :partial => "stats", :locals => { :tweeter => tweeter }
+        end
+      end
+    end
+  end
 end
