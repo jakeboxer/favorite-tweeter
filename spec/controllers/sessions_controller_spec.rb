d6499e7e9b2866918e42f84969dbe1c4a478fa9a
Jake Boxer <jake@github.com>
2014-01-04 21:56:33 -0800

    Remove out-of-date controller specs

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,12 +1,4 @@
 require 'spec_helper'
 
 describe SessionsController do
-
-  describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
-      expect(response).to be_success
-    end
-  end
-
 end
